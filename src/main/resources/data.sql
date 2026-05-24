91185e001e760d7f13c7f0da96fb12dd7a0558eb
Binin Shrestha <bininshrestha@gmail.com>
2026-05-24 15:00:57 +0545

    Test using dummy data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO patient (name, gender, birth_date, email, blood_group)
+VALUES
+    ('Ram Shrestha', 'MALE', '2009-06-12', 'baka2@gmail.com', 'O_POSITIVE'),
+    ('Ram Shrestha', 'MALE', '2009-06-13', 'baka1@gmail.com', 'O_NEGATIVE'),
+    ('Ram Shrestha', 'MALE', '2009-06-14', 'baka3@gmail.com', 'B_POSITIVE'),
+    ('Ram Shrestha', 'MALE', '2009-06-04', 'baka4@gmail.com', 'A_NEGATIVE');
